1c5170ccef111cb37b933eb915a4b8ee6453f8bc
Samychowdhury01 <samychowdhury84@gmail.com>
2023-04-24 20:04:36 +0600

    right Nav design done

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { FaGoogle, FaGithub } from "react-icons/fa";
+import { ListGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagram,
+} from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
   return (
@@ -15,6 +24,32 @@ const RightNav = () => {
           login with Google
         </button>
       </div>
+      <div>
+        <h4 className="mt-4">Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebookSquare className="me-2 fs-2" />
+            Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitterSquare className="me-2 fs-2" />
+            Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram className="me-2 fs-2" />
+            Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone/>
+      <div className='position-relative'>
+                <img src={bg} alt="" />
+                <div className="position-absolute top-50 start-50 translate-middle text-white text-center">
+                    <h3 className='fw-bold'>Create an Amazing Newspaper</h3>
+                    <p>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+                    <button className='btn btn-danger'>Learn More</button>
+                </div>
+            </div>
     </div>
   );
 };
