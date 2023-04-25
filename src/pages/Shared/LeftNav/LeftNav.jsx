8b30b9f39a091bc457130d030d7d521e221b4210
Samychowdhury01 <samychowdhury84@gmail.com>
2023-04-25 20:12:32 +0600

    added right nav category list

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <h1>Left Nav</h1>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div>
+      <h4 className="mb-5">All Category</h4>
+      <div className="ps-4">
+        {categories.map((category) => (
+          <p key={category.id} className='mb-4'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-secondary bg-opacity-25 p-2 text-black" : "text-black text-decoration-none"
+              }
+              to={`/category/${category.id}`}
+            >
+              {category.name}
+            </NavLink>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
